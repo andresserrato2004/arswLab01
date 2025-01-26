0eca503ba9dfa2b3283e01a1267c674b496deba6
andresserrato2004 <112002636+andresserrato2004@users.noreply.github.com>
2025-01-26 13:03:25 -0500

    feat(threads): added method run in class counthread and created threads en Main class

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -12,7 +12,19 @@ package edu.eci.arsw.threads;
 public class CountThreadsMain {
     
     public static void main(String a[]){
-        
+
+        CountThread Thread1 = new CountThread(0, 99);    
+        CountThread Thread2 = new CountThread(99, 199);
+        CountThread Thread3 = new CountThread(200, 299);
+    /* 
+        Thread1.start();
+        Thread2.start();
+        Thread3.start();
+     */
+
+        Thread1.run();
+        Thread2.run();
+        Thread3.run();
     }
     
 }
